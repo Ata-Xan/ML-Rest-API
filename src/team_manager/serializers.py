daf73b6b6873d8d494677ea0538ebeb390699266
ata xan <ataa1370@gmail.com>
2023-05-23 23:05:53 +0200

    changes

--- src/team_manager/serializers.py
+++ src/team_manager/serializers.py
@@ -1,17 +1,41 @@
 from rest_framework import serializers
 from .models import User, Team, Membership
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import RefreshToken
+
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    refresh = serializers.SerializerMethodField()
+    access = serializers.SerializerMethodField()
+
     class Meta:
         model = User
-        fields = '__all__'
+        fields = ['id', 'email', 'refresh', 'access']
 
-class TeamSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Team
-        fields = '__all__'
+    def get_refresh(self, obj):
+        refresh = RefreshToken.for_user(obj)
+        return str(refresh)
 
-class MembershipSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Membership
-        fields = '__all__'
+    def get_access(self, obj):
+        refresh = RefreshToken.for_user(obj)
+        return str(refresh.access_token)
+
+
+
+# class TeamSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Team
+#         fields = '__all__'
+
+# class MembershipSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Membership
+#         fields = '__all__'
--- src/team_manager/urls.py
+++ src/team_manager/urls.py
@@ -1,18 +1,23 @@
 from django.urls import path, include
 from rest_framework import routers
-from .viewsets import UserViewSet, TeamViewSet, MembershipViewSet, teams_list, memberships_list, user
-from .views import LoginView, LogoutView, RegisterView
+from .viewsets import LoginViewSet, LogoutViewSet, RegistrationViewSet
+# from .views import LoginView, LogoutView, RegisterView
+
 
 
 router = routers.DefaultRouter()
+router.register(r'register', RegistrationViewSet, basename='register')
+router.register(r'login', LoginViewSet, basename='login')
+router.register(r'logout', LogoutViewSet, basename='logout')
 
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('user/', user, name='user'),
-    path('teams/',teams_list,name='teams'),
-    path('memberships/', memberships_list, name='memberships'),
-    path('login/', LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', RegisterView.as_view(), name='register'),
+
+    # path('user/', user, name='user'),
+    # path('teams/',teams_list,name='teams'),
+    # path('memberships/', memberships_list, name='memberships'),
+    # path('login/', LoginView.as_view(), name='login'),
+    # path('logout/', LogoutView.as_view(), name='logout'),
+    # path('register/', RegisterView.as_view(), name='register'),
 ]
