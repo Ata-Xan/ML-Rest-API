5b490ec38b1f8dde3b53f92d78c5715b4077af79
ata xan <ataa1370@gmail.com>
2023-05-24 11:46:08 +0200

    circleci changed

--- src/team_manager/tests.py
+++ src/team_manager/tests.py
@@ -5,7 +5,6 @@ from django.test import TestCase
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
-from rest_framework.exceptions import ErrorDetail
 from .models import User
 
 
